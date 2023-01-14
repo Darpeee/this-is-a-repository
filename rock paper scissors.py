1d2492e0b283b84ca1a64ce2503f8cb3b64ab78d
Darby <81409263+Darpeee@users.noreply.github.com>
2023-01-15 00:29:45 +0530

    Update rock paper scissors.py

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,73 +1,66 @@
-import random
 print('Best Of Three Rounds Of Rock/Paper/Scissors')
 
-def start():
-    you=comp=stupid=0
-    while you<2 and comp<2 and stupid<5:
-        x=('Rock','Paper','Scissors')
-        a = random.choice(x)
+def Stone_Paper_Scissors():
+    choice=['Stone','Paper','Scissors']
+    gali=['Sala','Randi','Madrchod','Bhen Ka Lund','Teri Maki','Chutiye','Sahi Se Khel','Bhaag Ja Madrchod']
+    import random
     
-        b = input('Choose Between Rock/Paper/Scissors: ')
-        b = b.capitalize()
-    
-        if a=="Rock":
-            if b=="Rock":
-                print("It's A Tie, Go Again")
-            elif b=="Paper":
-                print("You Won This Round")
-                you+=1
-            elif b=="Scissors":
-                print("You Lost This Round")
-                comp+=1
+    x=y=z=0
+    while x<3 and y<3:
+        
+        computer=random.choice(choice)
+        player=(input('Stone, Paper, Scissors?: '))
+        
+        if player.capitalize()=='Stone' or player.capitalize()=='Paper' or player.capitalize()=='Scissors':
+            z=0
+        if z==5:
+            print(random.choice(gali))
+            continue
+        
+        if computer=='Stone':
+            if player.capitalize()=='Stone':
+                print('Draw')
+            if player.capitalize()=='Paper':
+                print('Win')
+                x+=1
+            if player.capitalize()=='Scissors':
+                print('Lose')
+                y+=1
             else:
-                print("Please Enter Valid Input")
-                stupid+=1
-
-        elif a=="Paper":
-            if b=="Paper":
-                print("It's A Tie, Go Again")
-            elif b=="Scissors":
-                print("You Won This Round")
-                you+=1
-            elif b=="Rock":
-                print("You Lost This Round")
-                comp+=1
+                z+=1
+                
+        elif computer=='Paper':
+            if player.capitalize()=='Stone':
+                print('Lose')
+                y+=1
+            if player.capitalize()=='Paper':
+                print('Draw')
+            if player.capitalize()=='Scissors':
+                print('Win')
+                x+=1
             else:
-                print("Plase Enter Valid Input")
-                stupid+=1
-
-        elif a=="Scissors":
-            if b=="Scissors":
-                print("It's A Tie, Go Again")
-            elif b=="Rock":
-                print("You Won This Round")
-                you+=1
-            elif b=="Paper":
-                print("You Lost This Round")
-                comp+=1
+                z+=1
+                
+        elif computer=='Scissors':
+            if player.capitalize()=='Stone':
+                print('Win')
+                x+=1
+            if player.capitalize()=='Paper':
+                print('Lose')
+                y+=1
+            if player.capitalize()=='Scissors':
+                print('Draw')
             else:
-                print("Plase Enter Valid Input")
-                stupid+=1
+                z+=1
 
-    if you==2:
-        print("")
-        print('You Won')
-    
-    elif comp==2:
-        print("")
-        print("You Lost")
-    
-    elif stupid==5:
-        print("")
-        z=('Randi Ho Tum','Chutiye Bhaag Ja','Sala','Apki Ma Ki Chut :3','NOOOOOOOOOOO')
-        print(random.choice(z))
-    
+    if x>=3:
+        print('You Win')
+    elif y>=3:
+        print('You Lose')
     else:
-        print("Something Unpexted Happened")
-
-    again = input('Wanna Go Again Y/N: ')
-    start() if again.upper()=="Y" else print('Exiting') if again.upper()=="N" else print("Input Not Recognised, Exiting Anyway")
-
-start()
+        print('Something Unexpected Happened')
+        
+    umm=input('Play Again? Y/N: ')   
+    Stone_Paper_Scissors() if umm.upper()=='Y' else print('Exiting....')
     
-    
+Stone_Paper_Scissors()
